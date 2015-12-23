889a9ae7ea6e098c94703424f98dd54fc7c6331d
Sagar Kumar <sagarfrndz@gmail.com>
2015-12-23 12:58:32 +0530

    New Test - First Class

---
+++ Jen_First/src/test/jen/First.java
@@ -0,0 +1,10 @@
+package test.jen;
+
+/**
+ * Created by Admin on 12/23/2015.
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Test Jen First");
+    }
+}
